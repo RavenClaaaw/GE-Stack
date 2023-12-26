6da1b14ae7b737154327826a8602b25f719cf7ce
Roop Kumar Lala <ui20cs60@iiitsurat.ac.in>
2023-12-27 00:51:11 +0530

    Push

---
+++ Stack/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Stack<Integer> stk = new Stack<>();
+
+        stk.push(1);
+        stk.push(2);
+        stk.push(3);
+    }
+}
